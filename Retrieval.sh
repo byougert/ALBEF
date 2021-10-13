11126624687481e8c769b2e9505c92a21770519c
byougert <byougert@qq.com>
2021-10-13 20:12:00 +0800

    set visible devices

--- Retrieval.sh
+++ Retrieval.sh
@@ -1,6 +1,8 @@
 cd /home/byyoung/python_space/ALBEF
 
-python -m torch.distributed.launch --nproc_per_node=8 --use_env Retrieval.py \
+CUDA_VISIBLE_DEVICES=4,5,6,7
+
+python -m torch.distributed.launch --nproc_per_node=4 --use_env Retrieval.py \
 --config ./configs/Retrieval_flickr.yaml \
 --output_dir output/Retrieval_flickr \
 --checkpoint /data/ALBEF/flickr30k.pth \
