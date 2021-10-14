c5f5b132c289f4ed2da7a687b55938891675beba
byougert <byougert@qq.com>
2021-10-14 14:59:58 +0800

    change model checkpoint

--- Retrieval.sh
+++ Retrieval.sh
@@ -5,5 +5,5 @@ CUDA_VISIBLE_DEVICES=4,5,6,7
 python -m torch.distributed.launch --nproc_per_node=4 --use_env Retrieval.py \
 --config ./configs/Retrieval_flickr.yaml \
 --output_dir output/Retrieval_flickr \
---checkpoint /data/ALBEF/flickr30k.pth \
+--checkpoint /data/ALBEF/ALBEF.pth \
 --evaluate
