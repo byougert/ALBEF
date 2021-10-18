f062189ec98bd0898d19517e9dd520acf8b9bb6c
byougert <byougert@qq.com>
2021-10-18 10:32:59 +0800

    choose top_K by inversion list

--- Retrieval_eval.py
+++ Retrieval_eval.py
@@ -21,8 +21,10 @@ from models.vit import interpolate_pos_embed
 from optim import create_optimizer
 from scheduler import create_scheduler
 
+
 @torch.no_grad()
-def evaluate(model, data_loader, tokenizer, device, config):
+def evaluation(model, data_loader, tokenizer, device, config):
+    # test
     model.eval()
 
     metric_logger = utils.MetricLogger(delimiter="  ")
@@ -33,69 +35,72 @@ def evaluate(model, data_loader, tokenizer, device, config):
 
     dataset = data_loader.dataset
     texts = dataset.text
+    num_text = len(texts)
     text_bs = 256
+    text_feats = []
+    text_embeds = []
+    text_atts = []
+    for i in range(0, num_text, text_bs):
+        # list: 256
+        text = texts[i: min(num_text, i + text_bs)]
+        # text_input.input_ids: 256 * 30, text_input.attention_mask: 256 * 30
+        text_input = tokenizer(text, padding='max_length', truncation=True, max_length=30, return_tensors="pt").to(
+            device)
+        text_output = model.text_encoder(text_input.input_ids, attention_mask=text_input.attention_mask, mode='text')
+        # text_feat: 256 * 30 * 768
+        text_feat = text_output.last_hidden_state
+        # text_embed: 256 * 256
+        text_embed = F.normalize(model.text_proj(text_feat[:, 0, :]))
+        text_embeds.append(text_embed)
+        text_feats.append(text_feat)
+        text_atts.append(text_input.attention_mask)
+    # val: text_embeds: 5070 * 256
+    text_embeds = torch.cat(text_embeds, dim=0)
+    # val: text_feats: 5070 * 30 * 256
+    text_feats = torch.cat(text_feats, dim=0)
+    # val: text_atts: 5070 * 30
+    text_atts = torch.cat(text_atts, dim=0)
 
-    score_matrix_i2t = torch.full((len(data_loader.dataset.image), len(texts)), -100.0).to(device)
-
-    img2text = json.load(open(config['img2txt_file'], 'r'))
-    for img_id, captions_id in metric_logger.log_every(img2text.items(), 5, header):
-        img_id = int(img_id)
-        order = dataset.img_id2order[img_id]
-        image, index = dataset[order]
+    image_feats = []
+    image_embeds = []
+    for image, img_order in data_loader:
+        # image: 64 * 3 * 384 * 384
         image = image.to(device)
-        # image: 1 * 3 * 384 * 384
-        image = image.unsqueeze(dim=0)
-        # image_feat: 1 * 577 * 768
+        # image_feat: 64 * 577 * 768
         image_feat = model.visual_encoder(image)
-        # image_embed: 1 * 256
+        # image_embed: 64 * 256
         image_embed = model.vision_proj(image_feat[:, 0, :])
         image_embed = F.normalize(image_embed, dim=-1)
 
-        num_text = len(captions_id)
-        captions_order = [dataset.text_id2order[cap_id] for cap_id in captions_id]
-        text_feats = []
-        text_embeds = []
-        text_atts = []
+        image_feats.append(image_feat)
+        image_embeds.append(image_embed)
 
-        for i in range(0, num_text, text_bs):
-            # list: 256
-            text = [texts[order] for order in captions_order[i: min(num_text, i + text_bs)]]
-            # text_input.input_ids: 256 * 30, text_input.attention_mask: 256 * 30
-            text_input = tokenizer(text, padding='max_length', truncation=True, max_length=30,
-                                   return_tensors="pt").to(device)
-            text_output = model.text_encoder(text_input.input_ids, attention_mask=text_input.attention_mask,
-                                             mode='text')
-            # text_feat: 256 * 30 * 768
-            text_feat = text_output.last_hidden_state
-            # text_embed: 256 * 256
-            text_embed = F.normalize(model.text_proj(text_feat[:, 0, :]))
-            text_embeds.append(text_embed)
-            text_feats.append(text_feat)
-            text_atts.append(text_input.attention_mask)
-        # val: text_embeds: 1819 * 256
-        text_embeds = torch.cat(text_embeds, dim=0)
-        # val: text_feats: 1819 * 30 * 256
-        text_feats = torch.cat(text_feats, dim=0)
-        # val: text_atts: 1819 * 30
-        text_atts = torch.cat(text_atts, dim=0)
+    # val: image_feats: 1014 * 577 * 768
+    image_feats = torch.cat(image_feats, dim=0)
+    # val: image_embeds: 1014 * 256
+    image_embeds = torch.cat(image_embeds, dim=0)
 
-        # val: sims_matrix: 1 * 1819
-        sims_matrix = image_embed @ text_embeds.t()
-        # val: score_matrix_i2t: 1819
-        score_matrix_i2t = torch.full((num_text,), -100.0).to(device)
+    # val: sims_matrix: 1014 * 5070
+    sims_matrix = image_embeds @ text_embeds.t()
+    # val: score_matrix_i2t: 1014 * 5070
+    score_matrix_i2t = torch.full((len(data_loader.dataset.image), len(texts)), -100.0).to(device)
 
-        num_tasks = utils.get_world_size()
-        rank = utils.get_rank()
-        step = sims_matrix.size(0) // num_tasks + 1
-        start = rank * step
-        end = min(sims_matrix.size(0), start + step)
+    num_tasks = utils.get_world_size()
+    rank = utils.get_rank()
+    step = sims_matrix.size(0) // num_tasks + 1
+    start = rank * step
+    end = min(sims_matrix.size(0), start + step)
 
-        sims = sims_matrix[0]
+    img2text = json.load(open(config['img2text_file'], 'r'))
+    text2img = json.load(open(config['text2img_file'], 'r'))
+
+    for i, sims in enumerate(metric_logger.log_every(sims_matrix[start:end], 50, header)):
         # sims: 5070 topk_sim: 128 topk_idx: 128
-        topk_sim, topk_idx = sims.topk(k=config['k_test'], dim=0)
+        # topk_sim, topk_idx = sims.topk(k=config['k_test'], dim=0)
+        topk_idx = [dataset.img_id2order[j] for j in img2text[str(dataset.img_order2id[i])]]
 
         # encoder_output: 128 * 577 * 768
-        encoder_output = image_feat.repeat(config['k_test'], 1, 1)
+        encoder_output = image_feats[start + i].repeat(len(topk_idx), 1, 1)
         # encoder_att: 128 * 577
         encoder_att = torch.ones(encoder_output.size()[:-1], dtype=torch.long).to(device)
         # text_feats[topk_idx]: 128 * 30 * 768, text_atts[topk_idx]: 128 * 30,
@@ -109,11 +114,40 @@ def evaluate(model, data_loader, tokenizer, device, config):
                                     )
         # score: list 128
         score = model.itm_head(output.last_hidden_state[:, 0, :])[:, 1]
-        score_matrix_i2t[topk_idx] = score
+        score_matrix_i2t[start + i, topk_idx] = score
 
-        total_time = time.time() - start_time
-        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-        # print('Evaluation time {}'.format(total_time_str))
+    sims_matrix = sims_matrix.t()
+    score_matrix_t2i = torch.full((len(texts), len(data_loader.dataset.image)), -100.0).to(
+        device)  # score_matrix_t2i: 5070 * 1014
+
+    step = sims_matrix.size(0) // num_tasks + 1
+    start = rank * step
+    end = min(sims_matrix.size(0), start + step)
+
+    for i, sims in enumerate(metric_logger.log_every(sims_matrix[start:end], 50, header)):
+        topk_sim, topk_idx = sims.topk(k=config['k_test'], dim=0)
+        encoder_output = image_feats[topk_idx]
+        encoder_att = torch.ones(encoder_output.size()[:-1], dtype=torch.long).to(device)
+        output = model.text_encoder(encoder_embeds=text_feats[start + i].repeat(config['k_test'], 1, 1),
+                                    attention_mask=text_atts[start + i].repeat(config['k_test'], 1),
+                                    encoder_hidden_states=encoder_output,
+                                    encoder_attention_mask=encoder_att,
+                                    return_dict=True,
+                                    mode='fusion'
+                                    )
+        score = model.itm_head(output.last_hidden_state[:, 0, :])[:, 1]
+        score_matrix_t2i[start + i, topk_idx] = score
+
+    if args.distributed:
+        dist.barrier()
+        torch.distributed.all_reduce(score_matrix_i2t, op=torch.distributed.ReduceOp.SUM)
+        torch.distributed.all_reduce(score_matrix_t2i, op=torch.distributed.ReduceOp.SUM)
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Evaluation time {}'.format(total_time_str))
+
+    return score_matrix_i2t.cpu().numpy(), score_matrix_t2i.cpu().numpy()
 
 
 @torch.no_grad()
@@ -239,7 +273,7 @@ def main(args, config):
     start_time = time.time()
 
     # score_val_i2t, score_val_t2i, = evaluation(model_without_ddp, val_loader, tokenizer, device, config)
-    score_result_i2t, score_result_t2i = evaluate(model_without_ddp, test_loader, tokenizer, device, config)
+    score_result_i2t, score_result_t2i = evaluation(model_without_ddp, test_loader, tokenizer, device, config)
 
     if utils.is_main_process():
         test_result = itm_eval(score_result_i2t, score_result_t2i, test_loader.dataset.txt2img, test_loader.dataset.img2txt)
