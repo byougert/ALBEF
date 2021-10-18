757381733f55e896673a6bb4bc009a8de19d84b7
byougert <byougert@qq.com>
2021-10-18 21:35:09 +0800

    distributed evaluation

--- Retrieval_eval.sh
+++ Retrieval_eval.sh
@@ -3,9 +3,9 @@ cd /home/byyoung/python_space/ALBEF
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 python -m torch.distributed.launch --nproc_per_node=4 --use_env \
-Retrieval.py \
---config ./configs/Retrieval_flickr.yaml \
---output_dir output/Retrieval_flickr \
+Retrieval_eval.py \
+--config ./configs/Retrieval_flickr_eval.yaml \
+--output_dir output/Retrieval_flickr_eval \
 --checkpoint /data/ALBEF/ALBEF.pth \
 --dist_url tcp://127.0.0.1:2222 \
 --evaluate
