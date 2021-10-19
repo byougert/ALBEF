df3891d130e96e702175f28715a893467c05edc5
byougert <byougert@qq.com>
2021-10-19 09:02:44 +0800

    change visible devices

--- Retrieval_eval.sh
+++ Retrieval_eval.sh
@@ -1,8 +1,8 @@
 cd /home/byyoung/python_space/ALBEF
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=4
 
-python -m torch.distributed.launch --nproc_per_node=4 --use_env \
+python -m torch.distributed.launch --nproc_per_node=1 --use_env \
 Retrieval_eval.py \
 --config ./configs/Retrieval_flickr_eval.yaml \
 --output_dir output/Retrieval_flickr_eval \
