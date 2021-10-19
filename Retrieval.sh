6ed3aeae1e85b7f4fa0a9f151cf8a67cbc2d823a
byougert <byougert@qq.com>
2021-10-19 16:46:32 +0800

    flickr finetuned evaluation

--- Retrieval.sh
+++ Retrieval.sh
@@ -6,5 +6,5 @@ python -m torch.distributed.launch --nproc_per_node=4 --use_env \
 Retrieval.py \
 --config ./configs/Retrieval_flickr.yaml \
 --output_dir output/Retrieval_flickr \
---checkpoint /data/ALBEF/mscoco.pth \
+--checkpoint /data/ALBEF/flickr30k.pth \
 --dist_url tcp://127.0.0.1:2222
