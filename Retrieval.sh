7a5780cd741bf5ff64f89d7163a1358df3290e28
byougert <byougert@qq.com>
2021-10-20 21:58:04 +0800

    evaluation on coco

--- Retrieval.sh
+++ Retrieval.sh
@@ -4,8 +4,8 @@ export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 python -m torch.distributed.launch --nproc_per_node=4 --use_env \
 Retrieval.py \
---config ./configs/Retrieval_flickr.yaml \
---output_dir output/Retrieval_flickr \
---checkpoint /data/ALBEF/flickr30k.pth \
+--config ./configs/Retrieval_coco.yaml \
+--output_dir output/Retrieval_coco \
+--checkpoint /data/ALBEF/mscoco.pth \
 --dist_url tcp://127.0.0.1:2222 \
 --evaluate
