1913e832dd356a9b1ce9f5653d8c5d4ab86cc510
byougert <byougert@qq.com>
2021-10-20 19:25:44 +0800

    rename eval to inv

--- Retrieval_inv.sh
+++ Retrieval_inv.sh
@@ -3,7 +3,7 @@ cd /home/byyoung/python_space/ALBEF
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 python -m torch.distributed.launch --nproc_per_node=4 --use_env \
-Retrieval_eval.py \
+Retrieval_inv.py \
 --config ./configs/Retrieval_flickr_eval.yaml \
 --output_dir output/Retrieval_flickr_eval \
 --checkpoint /data/ALBEF/mscoco.pth \
